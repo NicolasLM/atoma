7e457345cf8531c55212a61bb150f620b52b204b
Nicolas Le Manchet <nicolas@lemanchet.fr>
2020-11-17 18:20:49 +0100

    Run tests on more Python versions

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Text Processing :: Markup :: XML'
     ],
     keywords='atom rss json feed feeds syndication parser RFC4287',
